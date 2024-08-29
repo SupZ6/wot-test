1c2cf522a9c69c69fe98b6bf8d5bbca280dac6f2
风来 <heii@qq.com>
2024-08-29 20:00:01 +0800

    first commit

---
+++ src/pages/index/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <view class="wrap">
+    <wd-notice-bar :text="textArray1" @next="onNext1" />
+    <wd-gap />
+    <wd-notice-bar :text="textArray2" @next="onNext2" />
+    <wd-gap />
+    <wd-notice-bar :text="textArray3" />
+  </view>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const textArray1 = ref([
+  '用 ref 动态插入数据的 NoticeBar 通知栏补齐',
+  ''
+]);
+
+const textArray2 = [
+  '没有用 ref 动态插入数据的 NoticeBar 通知栏',
+  ''
+];
+
+const textArray3 = ref([
+  '静态数据的 NoticeBar 通知栏补齐补齐补齐补',
+  '2024-08-29T11:56:09.398Z'
+]);
+
+const onNext1 = () => {
+  const now = new Date();
+  textArray1.value[1] = now.toISOString();
+};
+
+const onNext2 = () => {
+  const now = new Date();
+  textArray2[1] = now.toISOString();
+};
+</script>
+
+<style lang="scss">
+.wrap {
+  padding: 60rpx;
+}
+</style>
