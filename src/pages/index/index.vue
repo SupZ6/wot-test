beab74685f1425655c69ef6fe7033413b558bff9
风来 <heii@qq.com>
2024-08-29 20:35:07 +0800

    update

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,10 +1,10 @@
 <template>
   <view class="wrap">
-    <wd-notice-bar :text="textArray1" @next="onNext1" />
+    <wd-notice-bar type="danger" :text="textArray1" @next="onNext1" />
     <wd-gap />
-    <wd-notice-bar :text="textArray2" @next="onNext2" />
+    <wd-notice-bar type="warning" :text="textArray2" @next="onNext2" />
     <wd-gap />
-    <wd-notice-bar :text="textArray3" />
+    <wd-notice-bar type="info" :text="textArray3" />
   </view>
 </template>
 
@@ -23,7 +23,7 @@ const textArray2 = [
 
 const textArray3 = ref([
   '静态数据的 NoticeBar 通知栏补齐补齐补齐补',
-  '2024-08-29T11:56:09.398Z'
+  '1999-09-09T09:09:09.999Z'
 ]);
 
 const onNext1 = () => {
